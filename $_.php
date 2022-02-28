9762de095a28495c5acca451afbbf0c977ba9d71
kevincasas <kevin@payrix.com>
2022-02-28 13:10:33 +0100

    reading index to the language

--- $_.php
+++ $_.php
@@ -1139,6 +1139,8 @@ class Template
 		}
 
 		$_SESSION['LANGUAGE_IN_USE'] = $language ? $language : self::$defaultLanguage;
+		$this->fullLanguage['FILES_BASE_PATH'] = FILES_BASE_PATH;
+		$this->fullLanguage['FILES_RELATIVE_PATH'] = FILES_RELATIVE_PATH;
 
 		return $this->fullLanguage;
 	}
