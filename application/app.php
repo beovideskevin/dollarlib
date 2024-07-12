fc39f673e789f68919ddb75a23d992f097af8e73
Kevin Beovides Casas <beovideskevin@gmail.com>
2024-07-12 17:30:35 -0400

    remove word

--- application/app.php
+++ application/app.php
@@ -7,7 +7,7 @@ function index($args = [])
 
 	$results = [
 		"EXAMPLE" => 'DollarLib',
-		"NOTES"   => 'Dollarlib is a simple php lib, or a very naive and flexible framework. I just use it for my own websites. Check the code at github: 
+		"NOTES"   => 'Dollarlib is a simple php lib, or a naive and flexible framework. I just use it for my own websites. Check the code at github: 
 					  <a href="https://github.com/beovideskevin/dollarlib" target="_blank">https://github.com/beovideskevin/dollarlib</a>.',
 		"OUTPUT"  => "<h4>README.md</h4><pre><code style='overflow: auto;'>" . htmlspecialchars(file_get_contents(FILES_ABSOLUTE_PATH . "/README.md")) ."</code></pre>" 
 	];
